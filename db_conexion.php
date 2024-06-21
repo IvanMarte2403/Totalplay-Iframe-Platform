aad6a127a349dee4f1190a64ea462f411cc64b1a
IvanMarte <ivanbiasfx@gmail.com>
2024-06-20 22:56:02 -0600

    update: game-1 configuration

--- db_conexion.php
+++ db_conexion.php
@@ -1,16 +1,16 @@
 
 <?php
- $conexion = new mysqli('localhost', 'bigtriviatotalpl_bigtrivia-new', 'M$rte24i', 'bigtriviatotalpl_TPGAh');
+//  $conexion = new mysqli('localhost', 'bigtriviatotalpl_bigtrivia-new', 'M$rte24i', 'bigtriviatotalpl_TPGAh');
 
- if ($conexion->connect_error) {
-     die("Conexion fallida: " . $conexion->connect_error);
- }
-
-
-//  $conexion = new mysqli('localhost', 'root', '', 'tpga');
-
-//  if ($conexion->connect_error) { 
-//      die("Conexión fallida: " . $conexion->connect_error);
+//  if ($conexion->connect_error) {
+//      die("Conexion fallida: " . $conexion->connect_error);
 //  }
+
+
+ $conexion = new mysqli('localhost', 'root', '', 'tpga');
+
+ if ($conexion->connect_error) { 
+     die("Conexión fallida: " . $conexion->connect_error);
+ }
 
 ?> 
