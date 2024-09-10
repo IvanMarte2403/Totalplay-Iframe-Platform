aa411aa1c46fa4dca8dfdb0685e5bc1807c1d75f
IvanMarte <ivanbiasfx@gmail.com>
2024-09-10 17:00:38 -0600

    update: 'Production 1.7'

--- db_conexion.php
+++ db_conexion.php
@@ -1,16 +1,16 @@
 
 <?php
-//  $conexion = new mysqli('localhost', 'bigtriviatotalpl_bigtrivia-new', 'M$rte24i', 'bigtriviatotalpl_TPGA');
+ $conexion = new mysqli('localhost', 'bigtriviatotalpl_bigtrivia-new', 'M$rte24i', 'bigtriviatotalpl_TPGA');
 
-//  if ($conexion->connect_error) {
-//      die("Conexion fallida: " . $conexion->connect_error);
-//  }
-
-
- $conexion = new mysqli('localhost', 'root', '', 'tpga');
-
- if ($conexion->connect_error) { 
-     die("Conexión fallida: " . $conexion->connect_error);
+ if ($conexion->connect_error) {
+     die("Conexion fallida: " . $conexion->connect_error);
  }
+
+
+//  $conexion = new mysqli('localhost', 'root', '', 'tpga');
+
+//  if ($conexion->connect_error) { 
+//      die("Conexión fallida: " . $conexion->connect_error);
+//  }
 
 ?> 
